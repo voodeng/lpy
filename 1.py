5cbc077318a1a3f1181fa3733a8e7fbaa8b8dda7
voodeng <voodeng@gmail.com>
2013-09-26 16:49:43 +0800

    day 2
    计算下个税
    了解print 输出手段，yep

--- 1.py
+++ 1.py
@@ -1,9 +1,20 @@
 #-*- coding: utf-8 -*-
-##coding: utf-8
-#中文注释问题，你敢信？？？两种方式都行
+# #coding: utf-8
+# 中文注释问题，你敢信？？？两种方式都行
 
 print "Whatever you doing?"
 print "Learn the Python,"
 print "and just learn 'How to input with mini keyboard, is so anti Human!'"
-print ' => 【 :( in day 1. 】'  #单双引号都可以表示字符串滴，字符串里边有‘ “ 或特殊字符当然得用\ 来转义，通用的。
+print ' => 【 :( in day 1. 】'  # 单双引号都可以表示字符串滴，字符串里边有‘ “ 或特殊字符当然得用\ 来转义，通用的。
 print 'i\'m a litter bird. 貌似有问题出现.....'
+
+print ("we are the \
+			rock") 	# 留意下二段文字的空格和缩进会显示
+print "thiserei is greed"\
+			"to see you agagen" # 3.0后不能用了
+print ("i see the world is "
+			"so beautily")
+longtext = ("i am the god\n"
+						"=>of the Vertil")
+print longtext
+# 唔，几种长字符串输入法
